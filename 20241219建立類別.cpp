8f32ac28e53591545cc368c0a6127d3cf717e2d6
ChenYu2865450611048 <313028@s.lhvs.tyc.edu.tw>
2024-12-19 15:24:19 +0800

    Update 20241219建立類別.cpp

--- 20241219建立類別.cpp
+++ 20241219建立類別.cpp
@@ -7,8 +7,18 @@ class myTime{
 		int minute;
 		void printTime();
 };
-
+void myTime::printTime(){
+    cout<<hour<<":"<<minute<<"\n";
+}
 int mian(){
-	
+   myTime now, open;
+   now.hour =8;
+   now.minute =18;
+   cout<<"現在時間";
+   now.printTime();
+   open.hour =9;
+   open.minute =10;
+   cout<<"開張時間";
+   open.printTime();
 	
 } 
